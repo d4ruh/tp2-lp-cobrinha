d3202667eb6cf10bfb845abda0af250130c3ae9c
RafaMendess <rafasilvam220@gmail.com>
2023-11-28 09:33:56 -0300

    Login page funcionando

--- src/main/LoginPanel.java
+++ src/main/LoginPanel.java
@@ -27,61 +27,57 @@ public class LoginPanel extends JPanel implements ActionListener {
         timer.start();
     }
     public void paintComponent(Graphics g) {
-        this.removeAll();
         super.paintComponent(g);
         draw(g);
     }
     public void draw(Graphics g) {
+        setLayout(new GridBagLayout());
 
-        g.setFont(new Font("Ink Free",Font.BOLD,60));
-        String login="Login";
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5); // Adjust spacing as needed
+
+        g.setFont(new Font("Ink Free", Font.BOLD, 60));
+        String login = "Login";
         FontMetrics metrics = getFontMetrics(g.getFont());
-        int x=((SCREEN_WIDTH - metrics.stringWidth(login))/2);
-        int y=SCREEN_HEIGHT/2 - SCREEN_HEIGHT/4;g.setColor(Color.gray);
-        g.drawString(login, x+5, y+5);
-        //titulo
+        int x = ((SCREEN_WIDTH - metrics.stringWidth(login)) / 2);
+        int y = SCREEN_HEIGHT / 2 - SCREEN_HEIGHT / 4;
+        g.setColor(Color.gray);
+        g.drawString(login, x + 5, y + 5);
+
+        // titulo
         g.setColor(Color.white);
         g.drawString(login, x, y);
-        g.setFont(new Font("Ink Free",Font.BOLD,20));
+        g.setFont(new Font("Ink Free", Font.BOLD, 20));
 
-        JTextField textField= new JTextField("Email");
-        JPasswordField passwordField= new JPasswordField("Senha");
-        Dimension textFieldSize = new Dimension(TILE_SIZE*6, TILE_SIZE);
+        JTextField textField = new JTextField("Email");
+        JPasswordField passwordField = new JPasswordField("Senha");
+        Dimension textFieldSize = new Dimension(TILE_SIZE * 6, TILE_SIZE);
+        textField.setPreferredSize(textFieldSize);
+        passwordField.setPreferredSize(textFieldSize);
 
-        textField.setFocusable(true);
-        passwordField.setFocusable(true);
-        textField.setEditable(true);
-        passwordField.setEditable(true);
-        this.add(textField);
-        textField.setVisible(true);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        add(textField, gbc);
 
-        textField.setSize(textFieldSize);
-        textField.setLocation(SCREEN_WIDTH/2 - TILE_SIZE*3, SCREEN_HEIGHT/2 - TILE_SIZE);
-        this.add(passwordField);
-        passwordField.setVisible(true);
+        gbc.gridy = 1;
+        add(passwordField, gbc);
 
-        passwordField.setSize(textFieldSize);
-        passwordField.setLocation(SCREEN_WIDTH/2 - TILE_SIZE*3, SCREEN_HEIGHT/2 + TILE_SIZE*2 - TILE_SIZE/2);
-
-
-        Dimension tamBttn = new Dimension(TILE_SIZE*6, TILE_SIZE);
+        Dimension tamBttn = new Dimension(TILE_SIZE * 6, TILE_SIZE);
 
         JButton bttn0 = new JButton("Login");
+        bttn0.setPreferredSize(tamBttn);
 
-        this.add(bttn0);
-        bttn0.setVisible(true);
-        bttn0.setSize(tamBttn);
-        bttn0.setLocation(SCREEN_WIDTH/2 - TILE_SIZE*3, SCREEN_HEIGHT/2 + TILE_SIZE*4 - TILE_SIZE/2);
+        gbc.gridy = 2;
+        add(bttn0, gbc);
 
-        bttn0.addActionListener(e->{
+        bttn0.addActionListener(e -> {
             System.out.println("Verificar login");
-//            User user= new User(textFieldSize.toString(),passwordField.toString());
+            User user = new User(textField.getText(), new String(passwordField.getPassword()));
+
+            System.out.println(user.getEmail());
+            System.out.println(user.getSenha());
             gf.changePage(1);
         });
-
-
-
-
     }
 
     @Override
